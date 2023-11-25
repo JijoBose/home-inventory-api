c6deca74717425d20a2b4289befad4e93c1c09d2
Jijo Bose <bosejijo@gmail.com>
2023-11-25 12:40:59 +0530

    refactored

--- src/api/home.rs
+++ src/api/home.rs
@@ -3,7 +3,7 @@ use diesel::{SqliteConnection, r2d2};
 use uuid::Uuid;
 
 use crate::actions;
-use crate::model::home;
+use crate::model;
 
 type DbPool = r2d2::Pool<r2d2::ConnectionManager<SqliteConnection>>;
 
@@ -11,7 +11,7 @@ type DbPool = r2d2::Pool<r2d2::ConnectionManager<SqliteConnection>>;
 async fn all_homes(pool: web::Data<DbPool>) -> Result<impl Responder> {
     let all_homes = web::block(move || {
       let mut conn = pool.get()?;
-      actions::find_all_homes(&mut conn)
+      actions::home::find_all_homes(&mut conn)
     })
     .await?
     .map_err(error::ErrorInternalServerError)?;
@@ -24,7 +24,7 @@ async fn find_home(pool: web::Data<DbPool>, home_id: web::Path<Uuid>) -> Result<
   let home_uid = home_id.into_inner();
   let home = web::block(move || {
     let mut conn = pool.get()?;
-    actions::find_home_by_uid(&mut conn, home_uid)
+    actions::home::find_home_by_uid(&mut conn, home_uid)
   })
   .await?
   .map_err(error::ErrorInternalServerError)?;
@@ -38,11 +38,11 @@ async fn find_home(pool: web::Data<DbPool>, home_id: web::Path<Uuid>) -> Result<
 #[post("/home")]
 async fn add_home(
   pool: web::Data<DbPool>,
-  form: web::Json<home::NewHome>,
+  form: web::Json<model::home::NewHome>,
 ) -> Result<impl Responder> {
   let home = web::block(move || {
     let mut conn = pool.get()?;
-    actions::insert_new_home(&mut conn, &form)
+    actions::home::insert_new_home(&mut conn, &form)
   })
   .await?
   .map_err(error::ErrorInternalServerError)?;
