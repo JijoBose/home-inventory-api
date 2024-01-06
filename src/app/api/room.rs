1acf7e77fa34735e026887fa06c25537cc222e5c
Jijo Bose <bosejijo@gmail.com>
2024-01-06 11:41:01 +0530

    List Rooms in a Home

--- src/app/api/room.rs
+++ src/app/api/room.rs
@@ -1,4 +1,4 @@
-use actix_web::{error, post, web, HttpResponse, Responder, Result};
+use actix_web::{error, get, post, web, HttpResponse, Responder, Result};
 use diesel::{r2d2, PgConnection};
 
 use crate::app::actions;
@@ -19,4 +19,21 @@ async fn add_room(
     .map_err(error::ErrorBadRequest)?;
 
     Ok(HttpResponse::Created().json(response))
+}
+
+#[get("/room")]
+async fn get_room(
+  pool: web::Data<DbPool>,
+  query: web::Query<models::room::RoomQuery>,
+) -> Result<impl Responder> {
+  let home_uid = query.home_id;
+
+  let response = web::block(move || {
+    let mut conn = pool.get()?;
+    actions::room::list_rooms(&mut conn, home_uid)
+  })
+  .await?
+  .map_err(error::ErrorBadRequest)?;
+
+  Ok(HttpResponse::Ok().json(response))
 }
