e8cc025cecb360a2e9d18e6ed20f96bf2d869785
Jijo Bose <bosejijo@gmail.com>
2024-01-15 23:16:07 +0530

    Axum and SeaORM Rewrite

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use axum::{
-  routing::{delete, get, post, patch},
+  routing::{delete, get, post},
   Router,
 };
 
@@ -13,7 +13,7 @@ pub fn create_routes(app_state: Arc<AppState>) -> Router {
   Router::new()
       .route("/houses", get(all_houses))
       .route("/houses", post(create_house))
-      .route("/houses/:id", patch(find_house))
+      .route("/houses/:id", get(find_house))
       .route("/houses/:id", delete(delete_house))
       .with_state(app_state)
 }
